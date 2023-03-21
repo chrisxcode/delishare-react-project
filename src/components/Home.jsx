66d1a4196ec2e157aabd78628ac77c13445ff075
Chris <xchrisxcodex@gmail.com>
2023-03-21 16:41:22 +0200

    Created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+
+export const Home = () => {
+
+    return (
+        <div className="homeWrapper">
+            <div className="homeContainer">
+                <h1>Welcome to DeliShare!</h1>
+            </div>
+        </div>
+
+
+    )
+
+
+}
