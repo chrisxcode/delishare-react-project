3021f2445d2d7b8a2441b4e81a6ff943914f50c1
Chris <xchrisxcodex@gmail.com>
2023-03-21 16:39:22 +0200

    Better code + added more routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { Route, Routes } from "react-router-dom"
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Auth } from './components/Auth';
-import { db } from './config/firebase';
+
+import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from "react-router-dom"
+
 import { getDocs, collection } from 'firebase/firestore';
+import { auth, db } from "./config/firebase"
+
+import { signUp, logIn, logout } from "./REST/account";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+import { Auth } from './components/Auth';
 import { ListOfRecipes } from './components/ListOfRecipes';
 
 function App() {
 
-    const [recipes, setRecipes] = useState([]);
+    // USER - Create state, which will hold user logged in status
+    const [loggedStatus, setLoggedStatus] = useState(auth?.currentUser);
 
+    // USER - Redirect to Home page when user logs in/out
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/");
+    }, [loggedStatus]);
+
+    // RECIPES - Connect to Firebase collection for recipes
     const recipeCollectionRef = collection(db, "recipes");
 
+    // RECIPES - Create state for recipe collection
+    const [recipes, setRecipes] = useState([]);
+
+    // RECIPES - Fetch recipes and store them in state (only on first load)
     const getRecipes = async () => {
         try {
             const data = await getDocs(recipeCollectionRef);
@@ -28,9 +48,24 @@ function App() {
 
     return (
         <div className="App">
+            <Header
+                loggedStatus={loggedStatus}
+                setLoggedStatus={setLoggedStatus}
+                signUp={signUp}
+                logIn={logIn}
+                logout={logout} />
             <Routes>
-                <Route path="/" element={<Auth />} />
+
+                <Route path="/" element={<Home />} />
+
+                <Route path="/auth" element={<Auth
+                    setLoggedStatus={setLoggedStatus}
+                    signUp={signUp}
+                    logIn={logIn}
+                    loggedStatus={loggedStatus} />} />
+
                 <Route path="/recipes" element={<ListOfRecipes recipes={recipes} />} />
+
             </Routes>
 
         </div >
