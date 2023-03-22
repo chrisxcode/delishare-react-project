5a7a388eab85556aa7a3215f0bf339d835f28aa9
Chris <xchrisxcodex@gmail.com>
2023-03-22 15:36:29 +0200

    Added navigation to details component

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -1,12 +1,19 @@
 import styles from './styles/SingleRecipe.module.css';
+import { Link } from "react-router-dom";
 
 export const SingleRecipe = ({ recipe }) => {
+
     return (
         <div className={styles.SingleRecipe}>
             <h1>{recipe.title}</h1>
             <p>Prep time: {recipe.prepTime} minutes</p>
             <p>Difficulty: {recipe.difficulty}</p>
             <p>{recipe.instructions}</p>
+            <div>
+                <ul>
+                    <li><Link className='li' to={recipe.id}>Details</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
