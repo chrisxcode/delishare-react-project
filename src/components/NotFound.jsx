63d2de486438e130a1665ea7354af25e1a18598e
Chris <xchrisxcodex@gmail.com>
2023-03-22 15:37:00 +0200

    Created basic 404 page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+
+
+export const NotFound = () => {
+
+    return (
+        <div className="notFoundWrapper">
+            <div className="notFoundContainer">
+                <h1>404</h1>
+                <h2>Not found</h2>
+            </div>
+        </div>
+    )
+}
