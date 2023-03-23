4658930ffb2e3563cbdfd4fb6b295c80306adc62
Chris <xchrisxcodex@gmail.com>
2023-03-23 18:50:07 +0200

    Moved the logout function in the profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,19 @@
 
 
-export const Profile = () => {
+export const Profile = ({
+    setLoggedStatus,
+    logout
+}) => {
+
+    const logOutHandler = () => {
+        logout(setLoggedStatus)
+    }
 
     return (
-        <h1>User Profile</h1>
+        <div>
+            <h1>User Profile</h1>
+            <li className="navLink"><button onClick={logOutHandler}>Logout</button></li>
+        </div>
+
     );
 }
