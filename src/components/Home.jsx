d1708ff799a692bff31241ac58d3e6d88ed1131c
Chris <xchrisxcodex@gmail.com>
2023-03-23 18:49:47 +0200

    Small change in Home component styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-
+import styles from "./styles/Home.module.css"
 
 export const Home = () => {
 
     return (
-        <div className="homeWrapper">
+        <div className={styles.home_wrapper}>
             <div className="homeContainer">
                 <h1>Welcome to DeliShare!</h1>
             </div>
