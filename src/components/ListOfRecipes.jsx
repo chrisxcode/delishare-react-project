7bf4ed4c1b21c865db94c6c18631addfca708607
Chris <xchrisxcodex@gmail.com>
2023-03-23 18:48:59 +0200

    Added filters and search in the Recipes Component

--- src/components/ListOfRecipes.jsx
+++ src/components/ListOfRecipes.jsx
@@ -1,12 +1,25 @@
 import { SingleRecipe } from "./SingleRecipe";
 import styles from "./styles/ListOfRecipes.module.css"
 
-export const ListOfRecipes = ({ recipes }) => {
+export const ListOfRecipes = ({ recipes, themeColors }) => {
 
     return (
-        <div>
-            <div className={styles.ListOfRecipes}>
-
+        <div className={styles.container}>
+            <div className={styles.search}>
+                <input type="text" />
+                <button>Search</button>
+            </div>
+            <div className={styles.filters}>
+                <h1>Filters</h1>
+                <label htmlFor="easy">Easy</label>
+                <input type="checkbox" id="easy" defaultValue="Easy" />
+                <label htmlFor="medium">Medium</label>
+                <input type="checkbox" id="medium" defaultValue="Medium" />
+                <label htmlFor="easy">Medium</label>
+                <input type="checkbox" id="hard" defaultValue="Hard" />
+            </div>
+            <div className={styles.recipes}>
+                <div className={styles.fade} style={{ backgroundImage: `linear-gradient(${themeColors.body}, transparent)` }}></div>
                 {recipes.map(recipe => (
                     <SingleRecipe key={recipe.id} recipe={recipe} />
                 ))}
