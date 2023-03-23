b24529e6ad232d1d23524006971fdb9fcda96716
Chris <xchrisxcodex@gmail.com>
2023-03-23 18:49:23 +0200

    New style for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,31 +2,26 @@ import { Link } from "react-router-dom";
 import styles from "./styles/Header.module.css";
 
 export const Header = ({
-    setLoggedStatus,
-    loggedStatus,
-    logout
+    themeColors,
+    loggedStatus
 }) => {
 
-    const logOutHandler = () => {
-        logout(setLoggedStatus)
-    }
-
     return (
-        <div className={styles.navContainer}>
-            <div className="userStatus">
-                {loggedStatus ? <p>- User is logged in -</p> : <p>- No user logged in -</p>}
-            </div>
-            <nav className="navigation">
-                <ul>
-                    <li className="navLink"><Link to="/">Home</Link></li>
-                    <li className="navLink"><Link to="/recipes">Recipes</Link></li>
+        <div className={styles.header} style={{ backgroundColor: themeColors.header }}>
+            <nav className={styles.navigation}>
+                <div className={styles.logo}>
+                    <Link to="/">DeliShare</Link>
+                </div>
+                <ul className={styles.links}>
+                    <li><Link className={styles.link} to="/">Home</Link></li>
+                    <li><Link className={styles.link} to="/recipes">Recipes</Link></li>
+                    <li><Link className={styles.link} to="/users">Users</Link></li>
+                    {loggedStatus && <li><Link className={styles.link} to="/create">Create</Link></li>}
+                </ul>
+                <ul className={styles.profile}>
                     {loggedStatus ?
-                        <>
-                            <li className="navLink"><Link to="/create">Create New Recipe</Link></li>
-                            <li className="navLink"><Link to="/profile">Profile</Link></li>
-                            <li className="navLink"><button onClick={logOutHandler}>Logout</button></li>
-                        </>
-                        : <li className="navLink"><Link to="/auth">Login</Link></li>}
+                        <li><Link className={styles.user} to="/profile">Profile</Link></li>
+                        : <li><Link className={styles.guest} to="/auth">Login</Link></li>}
                 </ul>
             </nav>
         </div>
